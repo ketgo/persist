b759317c503c21dae79a7a0011610f8afe3370d0
ketgo <ketangoyal1988@gmail.com>
2021-03-09 16:25:20 -0500

    Initial commit

---
+++ _persist/include/persist/core/page/log_page/log_page.hpp
@@ -0,0 +1,303 @@
+/**
+ * log_page.hpp - Persist
+ *
+ * Copyright 2021 Ketan Goyal
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef PERSIST_CORE_PAGE_LOG_PAGE_HPP
+#define PERSIST_CORE_PAGE_LOG_PAGE_HPP
+
+#include <unordered_map>
+
+#include <persist/core/exceptions.hpp>
+#include <persist/core/page/base.hpp>
+#include <persist/core/page/log_page/page_slot.hpp>
+
+namespace persist {
+
+// TODO:
+// 1. The insert is not thread safe as it exposes a loophole for
+// accessing protected slots by returning a pointer. We need a LogSlotHandle
+// which locks the access to slots from concurrent read and writes.
+// 2. Use move operations for writing page slots in page.
+
+/**
+ * @brief LogPage Class
+ *
+ * The log page is a page to store log records. Log records are persisted to
+ * backend storage by the log manager in pages for efficiency.
+ */
+class LogPage : public Page {
+public:
+  /**
+   * @brief Log Page Header
+   *
+   * The header contains page ID information.
+   */
+  class Header {
+  public:
+    /**
+     * @brief Page unique identifer
+     */
+    PageId page_id;
+
+    /**
+     * @brief Sequence number of the last log record in page.
+     *
+     * Note: A value of 0 indicates no complete or starting part of the log
+     * record found in page.
+     */
+    SeqNumber last_seq_number;
+
+    /**
+     * @brief Number of slots in the page
+     *
+     */
+    uint64_t slot_count;
+
+    /**
+     * @brief Storage size of the page.
+     */
+    size_t page_size;
+
+    /**
+     * @brief Construct a new Header object
+     *
+     */
+    Header(PageId page_id = 0, size_t page_size = DEFAULT_PAGE_SIZE)
+        : page_id(page_id), page_size(page_size), last_seq_number(0),
+          slot_count(0) {}
+
+    /**
+     * @brief Get storage size of header
+     *
+     */
+    size_t GetSize() {
+      return sizeof(PageId) + sizeof(SeqNumber) + sizeof(uint64_t) +
+             sizeof(Checksum);
+    }
+
+    /**
+     * Load object from byte string
+     *
+     * @param input input buffer span to load
+     */
+    void Load(Span input) {
+      if (input.size < GetSize()) {
+        throw PageParseError();
+      }
+      // Load bytes
+      persist::load(input, page_id, last_seq_number, slot_count);
+    }
+
+    /**
+     * Dump object as byte string
+     *
+     * @param output output buffer span to dump
+     */
+    void Dump(Span output) {
+      if (output.size < GetSize()) {
+        throw PageParseError();
+      }
+      // Dump bytes
+      persist::dump(output, page_id, last_seq_number, slot_count);
+    }
+
+#ifdef __PERSIST_DEBUG__
+    /**
+     * @brief Write page header to output stream
+     */
+    friend std::ostream &operator<<(std::ostream &os, const Header &header) {
+      os << "------- Header -------\n";
+      os << "id: " << header.page_id << "\n";
+      os << "lastSeqNumber: " << header.last_seq_number << "\n";
+      os << "slotCount: " << header.slot_count << "\n";
+      os << "----------------------";
+      return os;
+    }
+#endif
+  };
+
+  PERSIST_PRIVATE
+  /**
+   * @brief Page header
+   */
+  Header header;
+
+  /**
+   * @brief Mapping between sequence number and corresponding log record stored
+   * as bytes in log page slots.
+   */
+  typedef typename std::unordered_map<PageSlotId, LogPageSlot> SlotMap;
+  SlotMap slots;
+
+  /**
+   * @brief Size of free space available on the page.
+   */
+  uint64_t dataSize;
+
+public:
+  /**
+   * @brief Construct a new Log Page object
+   */
+  LogPage(PageId page_id = 0, size_t page_size = DEFAULT_LOG_PAGE_SIZE)
+      : header(page_id, page_size), dataSize(header.GetSize()) {
+    // Check page size greater than minimum size
+    if (page_size < MINIMUM_PAGE_SIZE) {
+      throw PageSizeError(page_size);
+    }
+  }
+
+  /**
+   * @brief Get the page type identifer.
+   *
+   * @returns The page type identifier
+   */
+  PageTypeId GetTypeId() const override { return 1; }
+
+  /**
+   * Get page identifier.
+   *
+   * @returns page identifier
+   */
+  const PageId &GetId() const override { return header.page_id; }
+
+  /**
+   * @brief Get the storage free space size in the page for specified operation.
+   *
+   * @param operation Operaion to be performed
+   * @returns Free space in bytes
+   */
+  size_t GetFreeSpaceSize(Operation operation) const override {
+    // If stored data size greater than page size then return 0
+    if (header.page_size <= dataSize) {
+      return 0;
+    }
+
+    return header.page_size - dataSize;
+  }
+
+  /**
+   * @brief Get the last sequence number in the page
+   *
+   * @returns Constant reference to last sequence number
+   */
+  const SeqNumber &GetLastSeqNumber() const { return header.last_seq_number; }
+
+  /**
+   * @brief Set the last sequence number in the page
+   *
+   * @param seq_number last sequence number in the page
+   */
+  void SetLastSeqNumber(SeqNumber seq_number) {
+    header.last_seq_number = seq_number;
+  }
+
+  /**
+   * Get page slot of given identifier within the page.
+   *
+   * @param seq_number sequence number of the log record seeked
+   * @returns Constant reference to the LogPageSlot object if found
+   * @throws PageSlotNotFoundError
+   */
+  const LogPageSlot &GetPageSlot(SeqNumber seq_number) const {
+    // Check if slot exists
+    SlotMap::const_iterator it = slots.find(seq_number);
+    if (it == slots.end()) {
+      throw PageSlotNotFoundError(header.page_id, seq_number);
+    }
+    return it->second;
+  }
+
+  /**
+   * @brief Insert log page slot to page.
+   *
+   * @param page_slot Reference to the LogPageSlot object to insert
+   * @returns pointer to the inserted LogPageSlot
+   */
+  LogPageSlot *InsertPageSlot(LogPageSlot &page_slot) {
+    // Update data size in page
+    dataSize += page_slot.GetSize();
+    // Insert record block at slot
+    auto inserted = slots.emplace(page_slot.GetSeqNumber(), page_slot);
+
+    return &inserted.first->second;
+  }
+
+  /**
+   * Load LogPage object from byte string.
+   *
+   * @param input input buffer span to load
+   */
+  void Load(Span input) override {
+    if (input.size < header.page_size) {
+      throw PageParseError();
+    }
+    slots.clear(); //<- clears data in case it is loaded
+
+    // Load Page header
+    header.Load(input);
+    dataSize = header.GetSize();
+    input += dataSize;
+    // Load bytes
+    persist::load(input, slots);
+  }
+
+  /**
+   * Dump LogPage object as byte string.
+   *
+   * @param output output buffer span to dump
+   */
+  void Dump(Span output) override {
+    if (output.size < header.page_size) {
+      throw PageParseError();
+    }
+    // Dump header
+    header.slot_count = slots.size();
+    header.Dump(output);
+    output += header.GetSize();
+    // Dump bytes
+    persist::dump(output, slots);
+    // Dump free space
+    std::memset((void *)output.start, 0, GetFreeSpaceSize(Operation::INSERT));
+  }
+
+#ifdef __PERSIST_DEBUG__
+  /**
+   * @brief Write page to output stream
+   */
+  friend std::ostream &operator<<(std::ostream &os, const LogPage &page) {
+    os << "--------- Page " << page.header.page_id << " ---------\n";
+    os << page.header << "\n";
+    for (auto element : page.slots) {
+      os << ":--> [" << page.header.page_id << ", " << element.first << "]\n";
+      os << element.second << "\n";
+    }
+    os << "-----------------------------";
+
+    return os;
+  }
+#endif
+};
+
+} // namespace persist
+
+#endif /* PERSIST_CORE_PAGE_LOG_PAGE_HPP */
---
+++ _persist/include/persist/core/page/log_page/page_slot.hpp
@@ -0,0 +1,304 @@
+/**
+ * log_page/page_slot.hpp - Persist
+ *
+ * Copyright 2021 Ketan Goyal
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef PERSIST_CORE_PAGE_LOG_PAGE_SLOT_HPP
+#define PERSIST_CORE_PAGE_LOG_PAGE_SLOT_HPP
+
+#include <persist/core/defs.hpp>
+#include <persist/core/exceptions.hpp>
+
+#include <persist/utility/serializer.hpp>
+
+namespace persist {
+/**
+ * @brief LogPageSlot Class
+ *
+ * The class implements a page slot used by log pages. In case a log record
+ * does not fit in a pge, it is split accross multiple slots.
+ */
+class LogPageSlot {
+public:
+  /**
+   * LogPageSlot Location Class
+   *
+   * The class object represents the address location of a slot in storage. This
+   * is simply the global unique identifier of the slot given by the tuple
+   * (PageId, SeqNumber).
+   */
+  struct Location {
+    /**
+     * @brief Identifier of the page containing slot.
+     */
+    PageId page_id;
+    /**
+     * @brief Sequence number of the log record stored in the slot.
+     */
+    SeqNumber seq_number;
+
+    /**
+     * Constructor
+     */
+    Location() : page_id(0), seq_number(0) {}
+    Location(PageId page_id, SeqNumber seq_number)
+        : page_id(page_id), seq_number(seq_number) {}
+
+    /**
+     * @brief Check if location is NULL. A NULL location will have both page ID
+     * and sequence number set to `0`.
+     */
+    bool IsNull() const { return page_id == 0 && seq_number == 0; }
+
+    /**
+     * @brief Set the location to NULL.
+     */
+    void SetNull() {
+      page_id = 0;
+      seq_number = 0;
+    }
+
+    /**
+     * @brief Equality comparision operator.
+     */
+    bool operator==(const Location &other) const {
+      return page_id == other.page_id && seq_number == other.seq_number;
+    }
+
+    /**
+     * @brief Non-equality comparision operator.
+     */
+    bool operator!=(const Location &other) const {
+      return page_id != other.page_id || seq_number != other.seq_number;
+    }
+
+#ifdef __PERSIST_DEBUG__
+    /**
+     * @brief Write slot location to output stream
+     */
+    friend std::ostream &operator<<(std::ostream &os,
+                                    const Location &location) {
+      os << "[" << location.page_id << ", " << location.seq_number << "]";
+      return os;
+    }
+#endif
+  };
+
+  /**
+   * LogPageSlot Header Class
+   *
+   * The class represents header of a plog age slot. It contains the metadata
+   * information required for facilitating read write operations of log records.
+   */
+  class Header {
+  public:
+    /**
+     * @brief Log record sequence number
+     */
+    SeqNumber seq_number;
+
+    /**
+     * @brief Next page slot location
+     */
+    Location next_location;
+
+    /**
+     * Constructors
+     */
+    Header() : seq_number(0) {}
+    Header(SeqNumber seq_number) : seq_number(seq_number) {}
+    Header(SeqNumber seq_number, Location next_location)
+        : seq_number(seq_number), next_location(next_location) {}
+
+    /**
+     * Load slot header from byte string.
+     *
+     * @param input input buffer span to load
+     */
+    void Load(Span input) {
+      if (input.size < sizeof(Header)) {
+        throw PageSlotParseError();
+      }
+      persist::load(input, seq_number, next_location);
+    }
+
+    /**
+     * Dump slot header as byte string.
+     *
+     * @param output output buffer span to dump
+     */
+    void Dump(Span output) {
+      if (output.size < sizeof(Header)) {
+        throw PageSlotParseError();
+      }
+      persist::dump(output, seq_number, next_location);
+    }
+
+    /**
+     * @brief Equality comparision operator.
+     */
+    bool operator==(const Header &other) const {
+      return seq_number == other.seq_number &&
+             next_location == other.next_location;
+    }
+
+    /**
+     * @brief Non-equality comparision operator.
+     */
+    bool operator!=(const Header &other) const {
+      return seq_number != other.seq_number ||
+             next_location != other.next_location;
+    }
+
+#ifdef __PERSIST_DEBUG__
+    /**
+     * @brief Write slot header to output stream
+     */
+    friend std::ostream &operator<<(std::ostream &os, const Header &header) {
+      os << "---- Header ----\n";
+      os << "seqNumber: " << header.seq_number << "\n";
+      os << "next: " << header.next_location << "\n";
+      os << "-----------------";
+      return os;
+    }
+#endif
+  };
+
+  PERSIST_PRIVATE
+  /**
+   * @brief LogPage slot header
+   *
+   */
+  Header header;
+
+public:
+  ByteBuffer data; //<- data contained in the record block
+
+  /**
+   * @brief Construct a new log page slot
+   *
+   */
+  LogPageSlot() {}
+  LogPageSlot(SeqNumber seq_number) : header(seq_number) {}
+  LogPageSlot(SeqNumber seq_number, Location next_location)
+      : header(seq_number, next_location) {}
+
+  /**
+   * @brief Size of fixed length members of slot
+   *
+   */
+  static const size_t fixedSize = sizeof(Header) + sizeof(size_t);
+
+  /**
+   * Get storage size of page slot.
+   */
+  size_t GetSize() const { return fixedSize + sizeof(Byte) * data.size(); }
+
+  /**
+   * @brief Get the sequence number of the stored log record
+   *
+   * @return sequence number of the stored log record
+   */
+  const SeqNumber &GetSeqNumber() const { return header.seq_number; }
+
+  /**
+   * @brief Set the sequence number of the stored log record
+   *
+   * @param seq_number sequence number of the stored log record
+   */
+  void SetSeqNumber(SeqNumber &seq_number) { header.seq_number = seq_number; }
+
+  /**
+   * @brief Get the next page slot location
+   *
+   * @return Location of the next linked page slot
+   */
+  const Location &GetNextLocation() const { return header.next_location; }
+
+  /**
+   * @brief Set the next page slot location
+   *
+   * @param location Location of the next linked page slot
+   */
+  void SetNextLocation(Location &location) { header.next_location = location; }
+
+  /**
+   * Load page slot object from byte string.
+   *
+   * @param input input buffer span to load
+   */
+  void Load(Span input) {
+    if (input.size < GetSize()) {
+      throw PageSlotParseError();
+    }
+    // Load header
+    header.Load(input);
+    // Load bytes
+    persist::load(input, data);
+  }
+
+  /**
+   * Dump page slot object as byte string.
+   *
+   * @param output output buffer span to dump
+   */
+  void Dump(Span output) {
+    if (output.size < GetSize()) {
+      throw PageSlotParseError();
+    }
+    // Dump header
+    header.Dump(output);
+    // Dump bytes
+    persist::dump(output, data);
+  }
+
+  /**
+   * @brief Equality comparision operator.
+   */
+  bool operator==(const LogPageSlot &other) const {
+    return header == other.header && data == other.data;
+  }
+
+  /**
+   * @brief Non-equality comparision operator.
+   */
+  bool operator!=(const LogPageSlot &other) const {
+    return header != other.header || data != other.data;
+  }
+
+#ifdef __PERSIST_DEBUG__
+  /**
+   * @brief Write record block to output stream
+   */
+  friend std::ostream &operator<<(std::ostream &os, const LogPageSlot &slot) {
+    os << "------ LogPageSlot ------\n";
+    os << slot.header << "\n";
+    os << "data: " << slot.data << "\n";
+    os << "--------------------------";
+    return os;
+  }
+#endif
+};
+
+} // namespace persist
+
+#endif /* PERSIST_CORE_PAGE_LOG_PAGE_SLOT_HPP */
